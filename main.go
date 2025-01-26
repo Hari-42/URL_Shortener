b6802638bf50b0694a2845ae778a26bc748894fd
Hari-42 <harisaranmohanathas@hotmail.com>
2025-01-26 22:05:43 +0100

    testing input and output

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type PageData struct {
+	URL string
+}
+
 func main() {
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
+		if r.Method == "POST" {
+			r.ParseForm()
+			url := r.FormValue("url")
 
+			tmpl := template.Must(template.ParseFiles("index.html"))
+			tmpl.Execute(w, PageData{URL: url})
+			fmt.Printf("URL entered: %s\n", url)
+		} else {
+			tmpl := template.Must(template.ParseFiles("index.html"))
+			tmpl.Execute(w, nil)
+		}
+	})
 	fmt.Println("Server is running on http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
